363c26b8b13dd6aff264f6aafab19771fd58d376
rich@richnoah.com <rich@richnoah.com>
2018-08-27 22:13:59 -0500

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Thank You</h1>
+    <p>I am still working my way through FreeCodeCamp but love learning.</p>
+    <p>It is amazing the opportunities that are available and the community that exists.</p>
+    <p>I am a seasoned Project Manager and the best projects are the ones that involve technology and the web</p>
+    <Link to="/page-2/">Go to page 2</Link>
+  </div>
+)
+
+export default IndexPage
---
+++ src/pages/page-2.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const SecondPage = () => (
+  <div>
+    <h1>You made it this far.</h1>
+    <p>Guess you are curious enough to see what else I have to say.</p>
+    <p>I would like a Golden Ticket Mr. Wonka.</p>
+    <Link to="/">Go back to the homepage</Link>
+  </div>
+)
+
+export default SecondPage
